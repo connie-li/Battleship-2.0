93bb95d4546057e88581b79559b34d5b2457fc0c
Connie <constanceli@ku.edu>
2019-09-30 18:35:53 -0500

    feat(overhaul): create Ship.h

---
+++ ship.h
@@ -0,0 +1,19 @@
+// ship.h
+#ifndef SHIP_H
+#define SHIP_H
+#include <string>
+using namespace std;
+
+/** Class that represents a Ship.
+ *
+ */
+class Ship
+{
+	private: 
+		int m_size;
+		string[] m_coords;
+
+	public:
+		Ship(int size);
+
+}
