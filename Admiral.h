abeac05ffc04d2a57c819efbe849a606984b8d72
Connie <constanceli@ku.edu>
2019-09-30 20:09:55 -0500

    feat(overhaul): add Admiral class member variable & method declarations

--- Admiral.h
+++ Admiral.h
@@ -5,8 +5,37 @@
 #include <string>
 #include <iostream>
 #include <stdexcept>
+#include "Grid.h"
+#include "Ship.h"
 using namespace std;
 
+class Admiral
+{
+	private:
+		Grid m_board;
+		int m_numShips;
+		Ship* m_fleet;
+		int m_numAfloat;
+		string m_playerName;
+		string* m_powerUpsArr;
 
+	public:
+		Admiral(int numShips);
+		~Admiral();
+
+		//getters
+		Grid* getBoard() const;
+		int getNumShips() const;
+		Ship* getFleet() const;
+		int getNumAfloat() const;
+		string getPlayerName() const;
+		string getPowerUps() const;
+
+		//setters
+
+
+		//helpers
+		void decNumAfloat();
+};
 
 #endif
