bd176ab4d0d2a78e9951a9504a55ae8f5e5d7597
Ethan <ethanlbrenner@gmail.com>
2019-10-06 23:55:44 -0500

    feat: outlined standard powerUp funcs and some getters/setters

---
+++ PowerUps.h
@@ -0,0 +1,55 @@
+#ifndef POWERUPS_H
+#define POWERUPS_H
+#include <string>
+#include <stdexcept>
+
+class PowerUps
+{
+    private:
+    //are we using an enum for the powerup storage (not ptr/array)?
+
+    //pointer for the list of aquired powerups
+    char* m_powerUpslist = nullptr;
+
+    public:
+
+    /**
+     * the default constructor that takes no parameters
+     * @param none
+     */
+    PowerUps();
+    ~PowerUps();
+
+    /**
+     * launches insta-kill torpedo
+     */
+    void useTorpedo();
+
+    /**
+     * reveals locations around the chosen coordinate
+     */
+    void useRadar();
+
+    /**
+     * hits random locations around the map
+     */
+    void useScatterShot();
+
+    /**
+     * seeks the smallest ship and fires upon it
+     */
+    void useUberCommander();
+
+    /**
+     * returns the list of collected powerups to the player
+     * @return a list of powerup symbols
+     */
+    char* getPowerUps();
+
+    /**
+     * adds a powerup in the player's arsenal
+     * @param char the char symbol used to repesent a specific power up
+     */
+    void addPowerUp(char symbol);
+};
+#endif
