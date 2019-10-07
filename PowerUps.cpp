2402d0fcc37664006f08d5a9fd5d5ada2c30d4f5
Ethan <ethanlbrenner@gmail.com>
2019-10-07 00:02:55 -0500

    feat: implemented constructor/destructor

--- PowerUps.cpp
+++ PowerUps.cpp
@@ -1,4 +1,12 @@
 #include "PowerUps.h"
 
+PowerUps::PowerUps()
+{
+    //initializes the power up list to an empty array
+    m_powerUpslist = new char[0];
+}
 
-
+PowerUps::~PowerUps(){
+    delete[] m_powerUpslist;
+    m_powerUpslist = nullptr;
+}
