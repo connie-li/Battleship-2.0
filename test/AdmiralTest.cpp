7c9a8d46dd2432b85ad9d70ff8da5aaf84962e7b
Connie <constanceli@ku.edu>
2019-10-16 13:13:21 -0500

    test(Admiral): test Adm & Ship setters/getters (WIP)

--- test/AdmiralTest.cpp
+++ test/AdmiralTest.cpp
@@ -14,6 +14,7 @@ int main()
 
 	// ADMIRAL TESTS
 	// test constructors & getters: CLEARED
+	cout << "-- TEST ADMIRAL CONSTRUCTORS & GETTERS --\n";
 	cout << "stackAdm1 has:\n";
 	cout << '\t' << stackAdm1.getNumShips() << " ships\n";
 	cout << '\t' << stackAdm1.getNumAfloat() << " afloat.\n";
@@ -35,6 +36,18 @@ int main()
 	cout << "\tfleet is empty = " << heapAdm2->getFleet().empty() << "\n";
 
 	// test setters & all getters
+	cout << "-- TEST ADMIRAL SETTERS, ADDING SHIPS, & ADMIRAL & SHIP GETTERS --\n";
+	cout << "Give stackAdm1 1 Ship at coord 1:1.\n";
+	string* coord1 = new string[1];
+	coord1[0] = "1:1";
+	stackAdm1.setNumShips(1);
+	stackAdm1.setNumAfloat(1);
+	stackAdm1.addShip(1, coord1);
+	cout << "stackAdm1 has:\n";
+	cout << '\t' << stackAdm1.getNumShips() << " ships\n";
+	cout << '\t' << stackAdm1.getNumAfloat() << " afloat.\n";
+	cout << "\tfleet is empty = " << stackAdm1.getFleet().empty() << "\n";
+	cout << "\ta Ship of size " << stackAdm1.getFleet()[0].getSize() << " at " << stackAdm1.getFleet()[0].getCoords()[0] << "\n";
 	
 
 	delete heapAdm1;
