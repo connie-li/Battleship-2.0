5e972f4e6bf27333dfccda083646232344cdd53a
Connie <constanceli@ku.edu>
2019-10-18 23:30:02 -0500

    test(Adm): (cont'd) upgrade Admiral testing functions

--- test/AdmiralTest.cpp
+++ test/AdmiralTest.cpp
@@ -14,76 +14,85 @@ void testInitial(string admName, Admiral* admPtr)
 	cout << "\tfleet is empty = " << admPtr->getFleet().empty() << "\n";
 }
 
-
-void runTests()
+void testAddShip(Admiral* admPtr)
 {
-	testInitial("heapAdm1", heapAdm1);
+	bool fleetSizeCorrect = false;
+	bool coordsCorrect = true;
 
+	cout << "Testing adding Ships\n: ";
+	string* coord1 = new string[1];
+	coord1[0] = "1:1";
+	string* coord5 = new string[5];
+	coord5[0] = "2:1";
+	coord5[1] = "2:2";
+	coord5[2] = "2:3";
+	coord5[3] = "2:4";
+	coord5[4] = "2:5";
+
+	admPtr->addShip(1, coord1);
+	admPtr->addShip(5, coord5);
+	vector<Ship*> tempFleet = admPtr->getFleet();
+
+	if(tempFleet.size() == 2)
+	{
+		fleetSizeCorrect = true;
+	}
+
+	if(tempFleet[0]->getCoords()[0] != coord1[0])
+	{
+		coordsCorrect = false;
+	}
+	for(int i = 0; i < 5; i++)
+	{
+		if(tempFleet[1]->getCoords()[i] != coord5[i])
+		{
+			coordsCorrect = false;
+		}
+	}
+
+	cout << "Fleet size: ";
+	if(fleetSizeCorrect)
+	{
+		cout << "PASSED\n";
+	}
+	else
+	{
+		cout << "FAILED\n";
+	}
+
+	cout << "Coords: ";
+	if(coordsCorrect)
+	{
+		cout << "PASSED\n";
+	}
+	else
+	{
+		cout << "FAILED\n";
+	}
+
+	delete[] coord1;
+	delete[] coord5;	
+}
+
+void testGetBoard(Grid* gridPtr)
+{
+	gridPtr->printGrid(false);
+
+}
+
+void runTests(Admiral* admPtr)
+{
+	testInitial("testAdm", admPtr);
+	testAddShip(admPtr);
+	testGetBoard(admPtr->getBoard());
 }
 
 int main()
 {
-	Admiral* heapAdm1 = new Admiral();
-	Admiral* heapAdm2 = new Admiral(4);
+	Admiral* testAdm = new Admiral(5);
 
-	runTests();
-	
-	// test setters & all getters
-	cout << "-- TEST ADMIRAL SETTERS, ADDING SHIPS, & ADMIRAL & SHIP GETTERS --\n";
-	cout << "Give stackAdm1 1 Ship at coord 1:1.\n";
-	string* coord1 = new string[1];
-	coord1[0] = "1:1";
-	stackAdm1.setNumShips(1);
-	stackAdm1.setNumAfloat(1);
-	stackAdm1.addShip(1, coord1);
-	cout << "stackAdm1 has:\n";
-	cout << '\t' << stackAdm1.getNumShips() << " ships\n";
-	cout << '\t' << stackAdm1.getNumAfloat() << " afloat.\n";
-	cout << "\tfleet is empty = " << stackAdm1.getFleet().empty() << "\n";
-	cout << "\ta Ship of size " << stackAdm1.getFleet()[0]->getSize() << " at " << stackAdm1.getFleet()[0]->getCoords()[0] << "\n";
+	runTests(testAdm);
 
-	cout << "\nGive stackAdm2 5 Ships, all horizontal.\n";
-	// string* stack2coords = generateCoords("1:1", 1, true);
-	// stackAdm2.addShip(1, stack2coords);
-	// string* stack2coords = generateCoords("2:1", 2, true);
-	// delete stack2coords;
-	// // stackAdm2.addShip(2, stack2coords);
-	// stack2coords = generateCoords("3:1", 3, true);
-	// // stackAdm2.addShip(3, stack2coords);
-	// stack2coords = generateCoords("4:1", 4, true);
-	// // stackAdm2.addShip(4, stack2coords);
-	// stack2coords = generateCoords("5:1", 5, true);
-	// stackAdm2.addShip(5, stack2coords);
-
-	string* stack2coords = new string[5];
-	stack2coords[0] = "1:1";
-	stack2coords[1] = "1:2";
-	stack2coords[2] = "1:3";
-	stack2coords[3] = "1:4";
-	stack2coords[4] = "1:5";
-	stackAdm2.addShip(5, stack2coords);
-	cout << "stackAdm2 has:\n";
-	cout << '\t' << stackAdm2.getNumShips() << " ships\n";
-	cout << '\t' << stackAdm2.getNumAfloat() << " afloat.\n";
-	cout << "\tfleet is empty = " << stackAdm2.getFleet().empty() << "\n";
-	cout << "\tShips of size:\n";
-	cout << "\t\t" << stackAdm2.getFleet()[0]->getSize() << '\n';
-	string* tempCoords = stackAdm2.getFleet()[0]->getCoords();
-	cout << "\t\tcoords: ";
-	for(int i = 0; i < 5; i++)
-	{
-		cout << tempCoords[i] << ", ";
-	}
-	cout << '\n';
-
-	// for(int i = 0; i < stackAdm2.getNumShips(); i++)
-	// {
-	// 	cout << "\t\t" << stackAdm2.getFleet()[i]->getSize() << " at coord " << stackAdm1.getFleet()[i]->getCoords()[i] << "\n";
-	// }
-
-	delete[] coord1;
-	delete[] stack2coords;
-	delete heapAdm1;
-	delete heapAdm2;
+	delete testAdm;
 	return(0);
 }
