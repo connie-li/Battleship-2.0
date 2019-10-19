856d424e0820ee0832580cd1a3b8ef04ff4b4e2c
Connie <constanceli@ku.edu>
2019-10-18 22:37:46 -0500

    test(Adm): upgrade Admiral testing based on Kyle's testing suite
    WIP

--- test/AdmiralTest.cpp
+++ test/AdmiralTest.cpp
@@ -5,78 +5,29 @@
 #include <string>
 using namespace std;
 
-/*
-string* generateCoords(string startCoord, int size, bool orientation)
+void testInitial(string admName, Admiral* admPtr)
 {
-	string* coordsArr = new string[size];
-	// parse startCoord
-	int row = stoi(startCoord.substr(0, 1), nullptr, 10);
-	int col = stoi(startCoord.substr(2, 1), nullptr, 10);
-
-	if(orientation)		// horizontal
-	{
-		int k = 0;
-		int l = col;
-
-		while((l <= col + size - 1) && (k < size))
-		{
-			coordsArr[k] = to_string(row) + ":" + to_string(l);
-			k++;
-			l++;
-		}
-	}
-	else	// vertical
-	{
-		int i = 0;
-		int j = row;
-		while((j <= row + size - 1) && (i < size))
-		{
-			coordsArr[i] = to_string(j) + ":" + to_string(col);
-			i++;
-			j++;
-		}
-	}
-
-	// print coords for verification
-	for(int x = 0; x < coordsArr->length(); x++)
-	{
-		cout << coordsArr[x] << ", ";
-	}
-	cout << '\n';
-	return(coordsArr);
+	cout << "Testing Admiral creation:\n";
+	cout << admName << " has:\n";
+	cout << '\t' << admPtr->getNumShips() << " ships\n";
+	cout << '\t' << admPtr->getNumAfloat() << " afloat.\n";
+	cout << "\tfleet is empty = " << admPtr->getFleet().empty() << "\n";
 }
-*/
+
+
+void runTests()
+{
+	testInitial("heapAdm1", heapAdm1);
+
+}
 
 int main()
 {
-	Admiral stackAdm1 = Admiral();
-	Admiral stackAdm2 = Admiral(5);
 	Admiral* heapAdm1 = new Admiral();
 	Admiral* heapAdm2 = new Admiral(4);
 
-	// ADMIRAL TESTS
-	// test constructors & getters: CLEARED
-	cout << "-- TEST ADMIRAL CONSTRUCTORS & GETTERS --\n";
-	cout << "stackAdm1 has:\n";
-	cout << '\t' << stackAdm1.getNumShips() << " ships\n";
-	cout << '\t' << stackAdm1.getNumAfloat() << " afloat.\n";
-	cout << "\tfleet is empty = " << stackAdm1.getFleet().empty() << "\n";
-
-	cout << "stackAdm2 has:\n";
-	cout << '\t' << stackAdm2.getNumShips() << " ships\n";
-	cout << '\t' << stackAdm2.getNumAfloat() << " afloat.\n";
-	cout << "\tfleet is empty = " << stackAdm2.getFleet().empty() << "\n";
-
-	cout << "heapAdm1 has:\n";
-	cout << '\t' << heapAdm1->getNumShips() << " ships\n";
-	cout << '\t' << heapAdm1->getNumAfloat() << " afloat.\n";
-	cout << "\tfleet is empty = " << heapAdm1->getFleet().empty() << "\n";
-
-	cout << "heapAdm2 has:\n";
-	cout << '\t' << heapAdm2->getNumShips() << " ships\n";
-	cout << '\t' << heapAdm2->getNumAfloat() << " afloat.\n";
-	cout << "\tfleet is empty = " << heapAdm2->getFleet().empty() << "\n";
-
+	runTests();
+	
 	// test setters & all getters
 	cout << "-- TEST ADMIRAL SETTERS, ADDING SHIPS, & ADMIRAL & SHIP GETTERS --\n";
 	cout << "Give stackAdm1 1 Ship at coord 1:1.\n";
